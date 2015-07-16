57b1d5ecf7818461daa08028241fb92dbe81f3a4
LarsCallenbach <lars.callenbach@gmx.de>
2015-07-16 20:54:18 +0200

    cleanup

--- QuantLibAddin/Addins/Calc/environment.sh
+++ QuantLibAddin/Addins/Calc/environment.sh
@@ -1,7 +1,7 @@
-export QL_PATH=/tmp/quantlib/QuantLib
-export OH_PATH=/tmp/quantlib/ObjectHandler
-export QA_PATH=/tmp/quantlib/QuantLibAddin
-export LG_PATH=/tmp/quantlib/log4cxx
+export QL_PATH=/work/QuantLib
+export OH_PATH=/work/ObjectHandler
+export QA_PATH=/work/QuantLibAddin
+export LG_PATH=/work/log4cxx
 export BOOST_PATH=/usr/lib64 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/quantlib/log4cxx/src/main/cpp/.libs/:$QA_PATH/qlo/.libs:$OH_PATH/oh/.libs:$QL_PATH/ql/.libs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LG_PATH/src/main/cpp/.libs/:$QA_PATH/qlo/.libs:$OH_PATH/oh/.libs:$QL_PATH/ql/.libs
 
